b43101a4c0537e8a72868f8294172567d91c76e9
Phillip Hsu <philliphsu@outlook.com>
2017-06-11 22:37:09 -0700

    Add javadoc to Themer APIs.

--- library/src/main/java/com/philliphsu/numberpadtimepicker/BottomSheetNumberPadTimePickerDialogThemer.java
+++ library/src/main/java/com/philliphsu/numberpadtimepicker/BottomSheetNumberPadTimePickerDialogThemer.java
@@ -19,42 +19,96 @@ public class BottomSheetNumberPadTimePickerDialogThemer extends NumberPadTimePic
         mTimePickerComponent = timePickerComponent;
     }
 
+    /**
+     * Set the background color of the FloatingActionButton.
+     *
+     * @param colors A ColorStateList that defines colors for {@link android.R.attr#state_enabled}.
+     *
+     * @see R.attr#nptp_fabBackgroundColor
+     */
     @Override
     public BottomSheetNumberPadTimePickerDialogThemer setFabBackgroundColor(ColorStateList colors) {
         mTimePickerComponent.setFabBackgroundColor(colors);
         return this;
     }
 
+    /**
+     * Set the ripple color of the FloatingActionButton.
+     *
+     * @see R.attr#nptp_fabRippleColor
+     */
     @Override
     public BottomSheetNumberPadTimePickerDialogThemer setFabRippleColor(@ColorInt int color) {
         mTimePickerComponent.setFabRippleColor(color);
         return this;
     }
 
+    /**
+     * Set the tint to apply to the FloatingActionButton's icon.
+     *
+     * @param tint A ColorStateList that defines colors for {@link android.R.attr#state_enabled}.
+     *
+     * @see R.attr#nptp_fabIconTint
+     */
     @Override
     public BottomSheetNumberPadTimePickerDialogThemer setFabIconTint(ColorStateList tint) {
         mTimePickerComponent.setFabIconTint(tint);
         return this;
     }
 
+    /**
+     * Set whether the FloatingActionButton should transition between its enabled and disabled colors.
+     *
+     * <p> If the {@link ShowFabPolicy} was set to {@link ShowFabPolicy#SHOW_FAB_VALID_TIME
+     * SHOW_FAB_VALID_TIME}, then setting this to {@code true} does nothing.
+     *
+     * @see #setFabBackgroundColor(ColorStateList)
+     * @see #setShowFabPolicy(int)
+     * @see R.attr#nptp_animateFabBackgroundColor
+     */
     @Override
     public BottomSheetNumberPadTimePickerDialogThemer setAnimateFabBackgroundColor(boolean animate) {
         mTimePickerComponent.setAnimateFabBackgroundColor(animate);
         return this;
     }
 
+    /**
+     * Set the policy for when the FloatingActionButton should be shown.
+     *
+     * @param policy {@link ShowFabPolicy#SHOW_FAB_ALWAYS SHOW_FAB_ALWAYS} or
+     *               {@link ShowFabPolicy#SHOW_FAB_VALID_TIME SHOW_FAB_VALID_TIME}.
+     *
+     * @see R.attr#nptp_showFab
+     */
     @Override
     public BottomSheetNumberPadTimePickerDialogThemer setShowFabPolicy(@ShowFabPolicy int policy) {
         mTimePickerComponent.setShowFabPolicy(policy);
         return this;
     }
 
+    /**
+     * Set whether the FloatingActionButton should animate onto the screen when the dialog is shown.
+     *
+     * <p> If the {@link ShowFabPolicy} was set to {@link ShowFabPolicy#SHOW_FAB_VALID_TIME
+     * SHOW_FAB_VALID_TIME}, then setting this to {@code true} does nothing.
+     *
+     * @see #setShowFabPolicy(int)
+     * @see R.attr#nptp_animateFabIn
+     */
     @Override
     public BottomSheetNumberPadTimePickerDialogThemer setAnimateFabIn(boolean animateIn) {
         mTimePickerComponent.setAnimateFabIn(animateIn);
         return this;
     }
 
+    /**
+     * Set the location of the backspace button.
+     *
+     * @param location {@link BackspaceLocation#LOCATION_HEADER LOCATION_HEADER} or
+     *                 {@link BackspaceLocation#LOCATION_FOOTER LOCATION_FOOTER}
+     *
+     * @see R.attr#nptp_backspaceLocation
+     */
     @Override
     public BottomSheetNumberPadTimePickerDialogThemer setBackspaceLocation(@BackspaceLocation int location) {
         mTimePickerComponent.setBackspaceLocation(location);
--- library/src/main/java/com/philliphsu/numberpadtimepicker/NumberPadTimePickerDialogThemer.java
+++ library/src/main/java/com/philliphsu/numberpadtimepicker/NumberPadTimePickerDialogThemer.java
@@ -21,48 +21,95 @@ public class NumberPadTimePickerDialogThemer implements NumberPadTimePickerTheme
         mTimePickerComponent = checkNotNull(timePickerComponent);
     }
 
+    /**
+     * Set the text color for the inputted time in the header.
+     *
+     * @see R.attr#nptp_inputTimeTextColor
+     */
     @Override
     public NumberPadTimePickerDialogThemer setInputTimeTextColor(@ColorInt int color) {
         mTimePickerComponent.setInputTimeTextColor(color);
         return this;
     }
 
+    /**
+     * Set the text color for the inputted AM/PM in the header.
+     *
+     * @see R.attr#nptp_inputAmPmTextColor
+     */
     @Override
     public NumberPadTimePickerDialogThemer setInputAmPmTextColor(@ColorInt int color) {
         mTimePickerComponent.setInputAmPmTextColor(color);
         return this;
     }
 
+    /**
+     * Set the tint to apply to the backspace icon.
+     *
+     * @param colors A ColorStateList that defines colors for {@link android.R.attr#state_enabled}.
+     *
+     * @see R.attr#nptp_backspaceTint
+     */
     @Override
     public NumberPadTimePickerDialogThemer setBackspaceTint(ColorStateList colors) {
         mTimePickerComponent.setBackspaceTint(colors);
         return this;
     }
 
+    /**
+     * Set the text color for the number keys.
+     *
+     * @param colors A ColorStateList that defines colors for {@link android.R.attr#state_enabled}.
+     *
+     * @see R.attr#nptp_numberKeysTextColor
+     */
     @Override
     public NumberPadTimePickerDialogThemer setNumberKeysTextColor(ColorStateList colors) {
         mTimePickerComponent.setNumberKeysTextColor(colors);
         return this;
     }
 
+    /**
+     * Set the text color for the alt keys, i.e. "AM"/"PM" for 12-hour mode and ":00"/":30"
+     * for 24-hour mode.
+     *
+     * @param colors A ColorStateList that defines colors for {@link android.R.attr#state_enabled}.
+     *
+     * @see R.attr#nptp_altKeysTextColor
+     */
     @Override
     public NumberPadTimePickerDialogThemer setAltKeysTextColor(ColorStateList colors) {
         mTimePickerComponent.setAltKeysTextColor(colors);
         return this;
     }
 
+    /**
+     * Set the background for the header containing the inputted time and AM/PM.
+     *
+     * @see R.attr#nptp_headerBackground
+     */
     @Override
     public NumberPadTimePickerDialogThemer setHeaderBackground(Drawable background) {
         mTimePickerComponent.setHeaderBackground(background);
         return this;
     }
 
+    /**
+     * Set the background for the number pad.
+     *
+     * @see R.attr#nptp_numberPadBackground
+     */
     @Override
     public NumberPadTimePickerDialogThemer setNumberPadBackground(Drawable background) {
         mTimePickerComponent.setNumberPadBackground(background);
         return this;
     }
 
+    /**
+     * Set the header divider.
+     *
+     * @see R.attr#nptp_divider
+     */
     @Override
     public NumberPadTimePickerDialogThemer setDivider(Drawable divider) {
         mTimePickerComponent.setDivider(divider);
